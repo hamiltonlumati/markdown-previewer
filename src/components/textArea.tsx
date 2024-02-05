b9f79b2fba5307726659c4f0588c9ab777bc8c0f
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-05 15:09:38 +0100

    Creation: textarea component

---
+++ src/components/textArea.tsx
@@ -0,0 +1,11 @@
+
+
+function TextArea(){
+    return(
+        <form>
+            <input type="text" className="" id="" />
+        </form>
+    )
+}
+
+export default TextArea;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import TextArea from './components/textArea';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <TextArea />
   </React.StrictMode>,
 )
