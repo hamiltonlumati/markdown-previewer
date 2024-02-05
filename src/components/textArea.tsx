73886b027fe83539c22dca85814a64c89678546f
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-05 15:58:26 +0100

    Update: textArea

--- src/components/textArea.tsx
+++ src/components/textArea.tsx
@@ -4,8 +4,13 @@ function TextArea(){
     return(
         <form className="">
             <label htmlFor="tetxArea" className="">Text</label>
-            <input type="text" className="border-2 rounded mx-2
-            " id="textArea" />
+            <textarea 
+                className= "border-2 rounded mx-2 w-1/2 h-36" 
+                id= "textArea"
+            />
+            <button type="submit" className="">
+                Submit
+            </button>
         </form>
     )
 }
