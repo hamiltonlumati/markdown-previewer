b930d0bde1bf90dd02541f03fbb3f76fb0cade3c
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-07 22:38:40 +0100

    React Markdown Installation and Component Creation

--- src/components/previewer.tsx
+++ src/components/previewer.tsx
@@ -1,12 +1,15 @@
 import type { RootState } from "../app/store";
 import { useSelector } from "react-redux";
+import Markdown from "react-markdown";
 
 function Previewer(){
-    const text = useSelector((state: RootState) => state.previewer.text)
 
+    const text = useSelector((state: RootState) => state.previewer.text)
     return(
         <div>
-            {text}
+            <Markdown>
+                {text}
+            </Markdown>
         </div>
     )
 
