6c2a4a598633ca65c429aa6cf41e1491cfabc305
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-07 09:57:41 +0100

    screen file update

--- src/layout/screen.tsx
+++ src/layout/screen.tsx
@@ -1,1 +1,12 @@
- 
+import TextArea from "../components/textArea";
+import Previewer from "../components/previewer";
+
+function Screen(){
+    return(
+        <div>
+            <TextArea />
+            <Previewer />
+        </div>
+    )
+}
+export default Screen;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import TextArea from './components/textArea';
-import Previewer from './components/previewer';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import Screen from './layout/screen';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <Provider store ={store}>
-        <TextArea />
-        <Previewer />
+        <Screen />
       </Provider>
   </React.StrictMode>,
 )
